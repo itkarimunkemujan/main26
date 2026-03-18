ede4db8cbc79fb86cbab97a4b51da012fe5d7373
Andreandhiki <andreandhikiriyantap@mail.ugm.ac.id>
2026-03-18 12:17:19 +0700

    feat: reikna typeface and hero bg

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@workspace/ui/lib/utils";
+import { cn } from "@workspace/ui/lib/utils"
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -23,8 +23,25 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", fontSans.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        fontSans.variable
+      )}
     >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Reikna&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,18 +2,43 @@ import { Button } from "@workspace/ui/components/button"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="text-muted-foreground font-mono text-xs">
-          (Press <kbd>d</kbd> to toggle dark mode)
+    <>
+      <div
+        className="flex min-h-svh items-end p-6 text-background"
+        style={{
+          backgroundImage: "url(/images/bg/hero-bg.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="flex min-w-0 flex-col gap-4">
+          <div>
+            <h1 className="font-display text-[10rem] leading-[0.5]">
+              Karimun Kemujan
+            </h1>
+            <p>You may now add components and start building.</p>
+            <p>We&apos;ve already added the button component for you.</p>
+            <Button className="mt-2">Button</Button>
+          </div>
+          <div className="font-mono text-xs text-muted-foreground">
+            (Press <kbd>d</kbd> to toggle dark mode)
+          </div>
         </div>
       </div>
-    </div>
+      <div className="min-[50svh[ flex items-center justify-center p-6">
+        {" "}
+        <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
+          <div>
+            <h1 className="font-medium">Project ready!</h1>
+            <p>You may now add components and start building.</p>
+            <p>We&apos;ve already added the button component for you.</p>
+            <Button className="mt-2">Button</Button>
+          </div>
+          <div className="font-mono text-xs text-muted-foreground">
+            (Press <kbd>d</kbd> to toggle dark mode)
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
